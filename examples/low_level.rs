84a4cf93b96e5bedf3304ece3d1bf01e3482ada6
Floris Bruynooghe <flub@devork.be>
2024-03-07 12:44:21 +0100

    clippy fixes

--- examples/low_level.rs
+++ examples/low_level.rs
@@ -81,7 +81,7 @@ async fn serve(acceptor: AcmeAcceptor, rustls_config: Arc<ServerConfig>, port: u
     }
 }
 
-const HELLO: &'static [u8] = br#"HTTP/1.1 200 OK
+const HELLO: &[u8] = br#"HTTP/1.1 200 OK
 Content-Length: 10
 Content-Type: text/plain; charset=utf-8
 
